5bfd6b4d4a3659a6dc5943e23a34297314a29f31
emmanuelguty <manuel15901@gmail.com>
2023-04-19 21:28:40 -0600

    Se añadio una funcion query para los estados de mexico

--- app/actions/getJets.ts
+++ app/actions/getJets.ts
@@ -2,19 +2,60 @@ import prisma from "@/app/libs/prismadb";
 
 export interface IJetsParams {
     userId?: string;
+    maxPeople?:number;
+    modelo?: number;
+    startDate?:string;
+    endDate?: string;
+    category?: string;
 }
 
 export default async function getJets(
     params: IJetsParams
 ) {
     try {
-        const { userId } = params;
+        const {
+            userId,
+            modelo,
+            maxPeople,
+            startDate,
+            endDate, 
+            category
+        } = params;
 
         let query: any = {};
 
         if (userId) {
             query.userId = userId;
         }
+
+        if (category) {
+            query.category = category;
+        }
+
+        if (modelo) {
+            query.modelo = {
+                gte: +modelo
+            }
+        }
+
+        if (startDate && endDate) {
+            query.NOT = {
+              reservations: {
+                some: {
+                  OR: [
+                    {
+                      endDate: { gte: startDate },
+                      startDate: { lte: startDate }
+                    },
+                    {
+                      startDate: { lte: endDate },
+                      endDate: { gte: endDate }
+                    }
+                  ]
+                }
+              }
+            }
+          }
 
         const jets = await prisma.jets.findMany({
             where: query,
